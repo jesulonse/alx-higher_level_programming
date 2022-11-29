4374f39ff8193b3a5262e40f271e8de3ae9bc60d
Adewale Isaac <jesulonse>
2022-11-29 10:46:21 -0800

    numbers to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if num < 10:
-        print('0{:d}'.format(num),end=", ")
+    if num == 99:
+        print('{:d}'.format(num),end="\n")
     else:
-        print('{:d}'.format(num),end=", ")
-        if num == 99:
-            print('{:d}'.format(num),end="\n")
+        print('{:02}'.format(num),end=", ")
