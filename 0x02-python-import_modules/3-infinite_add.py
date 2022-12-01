377d49e17cbab22b612c06c6cb585e5ad45d240c
Adewale Isaac <jesulonse>
2022-12-01 04:31:19 -0800

    all tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,9 +4,7 @@ if __name__ == "__main__":
     """this add all the arguments of the list"""
     import sys
     
-    count = len(sys.argv) - 1
     total = 0    
-    for i in range(count):
+    for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
-    
