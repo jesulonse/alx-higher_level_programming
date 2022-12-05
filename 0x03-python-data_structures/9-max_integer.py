72d36495b0fef8a7e127fd709ac443fac1b70cbe
Adewale Isaac <jesulonse>
2022-12-05 05:10:35 -0800

    all tasks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0
     if len(my_list) == 0:
         return None
     else:
+        max_int = my_list[0]
         for i in my_list:
             if i > max_int:
                 max_int = i
