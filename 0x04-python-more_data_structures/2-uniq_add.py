24365efdd3298a82f621d518e6bbb207f81f1b09
Adewale Isaac <jesulonse>
2022-12-07 23:58:36 -0800

    all tasks

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    #initializing the number as 0
-    numb = 0
-    for i in set(my_list):
-        numb += i
-    return numb
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
