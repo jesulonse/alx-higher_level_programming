6fbbb9ddfce3c969e8d0b81e102318839944d303
Adewale Isaac <jesulonse>
2022-12-21 03:46:16 -0800

    all tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Reps a square"""
+    def __init__(self, size):
+        """Initializing the squaere class
+        Args: size - size of the square defineed
+        """
+        self.__size = size
